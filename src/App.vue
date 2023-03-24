349ddb0785dfab3fd83f57a20ff4965856c6cefd
kira <c3bosskung@gmail.com>
2023-03-24 13:14:15 +0700

    edit app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <script setup>
-import {ref} from "vue";
-import InputModal from "@/component/InputModal.vue";
 import Navbar from "@/component/Navbar.vue";
 import Carousel from "@/component/Carousel.vue";
-import Card from "@/component/Card.vue";
-
 
 </script>
 
 <template>
   <Navbar/>
-  <Carousel/>
-  <InputModal/>
+<!--  <Carousel/>-->
 </template>
 
 <style scoped>
