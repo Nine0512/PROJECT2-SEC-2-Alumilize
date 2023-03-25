12ab0a87e1ff2cdee0dd3521bd4b35daf177c34f
kira <c3bosskung@gmail.com>
2023-03-26 03:48:21 +0700

    edit app.vue to original

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,15 @@
 <script setup>
+import {ref} from "vue";
+import InputModal from "@/component/InputModal.vue";
 import Navbar from "@/component/Navbar.vue";
 import Carousel from "@/component/Carousel.vue";
-import SignInPage from "@/view/SignInPage.vue";
-import InputModal from "@/component/InputModal.vue";
-import SignupPage from "@/view/SignupPage.vue";
-import AllBook from "@/view/AllBook.vue";
-
+import Card from "@/component/Card.vue";
 </script>
 
 <template>
-  <div data-theme="garden">
-<!--      <Carousel/>-->
-<!--      <InputModal/>-->
-<!--    <SignInPage/>-->
-<!--  <SignupPage/>-->
-<!--    <router-view/>-->
-<!--    <AllBook/>-->
-    <router-view/>
-  </div>
+  <Navbar/>
+  <Carousel/>
+  <InputModal/>
 </template>
-
 <style scoped>
-
 </style>
