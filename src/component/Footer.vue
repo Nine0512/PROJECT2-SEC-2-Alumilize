824d40fad5dc5f2fe659b570ff55aab6dc915d9b
nonybueno <88123596+nonybueno@users.noreply.github.com>
2023-03-25 23:04:18 +0700

    :lipstick: Add Footer

---
+++ src/component/Footer.vue
@@ -0,0 +1,106 @@
+<script setup></script>
+
+<template>
+  <div>
+    <div class="w-full bg-yellow-500 h-64 flex justify-center">
+      <div class="w-4/6 grid grid-rows-3 max-lg:hidden pb-2">
+        <div class="row-span-2">
+          <div class="grid grid-cols-4">
+            <div class="flex justify-end items-end h-40">
+              <img src="../../public/images/footerLogo.png" class="h-28" />
+            </div>
+            <div
+              class="flex justify-center items-center col-span-2 pt-5 relative"
+            >
+              <img
+                src="../../public/images/black-butler2-transformed.png"
+                class="h-36"
+              />
+              <div class="flex justify-center items-center pb-5">
+                <img
+                  src="../../public/images/qr-code.png"
+                  class="absolute h-28 drop-shadow-xl"
+                />
+              </div>
+            </div>
+            <div class="flex justify-center items-center text-lg">
+              <div>นอนอ่าน e-book สนุกกว่า!</div>
+            </div>
+          </div>
+          <hr class="border-gray-800" />
+        </div>
+        <div class="grid grid-cols-10">
+          <div class="col-span-2 flex justify-center items-center">
+            <div>
+              <div class="text-sm">Github Repo</div>
+              <div class="flex justify-center">
+                <a href="https://github.com/Nine0512/PROJECT2-SEC-2-Alumilize.git" target="_blank">
+                <img src="../../public/images/25231.png" class="h-14 w-14" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-span-6">
+            <div class="grid grid-cols-6">
+              <div class="flex justify-center text-sm items-center">
+                วิธีชำระเงิน
+              </div>
+              <div class="flex justify-center">
+                <img
+                  src="../../public/images/payment.png"
+                  class="h-9 pt-2 w-20 pl-1"
+                />
+              </div>
+              <div class="flex justify-center">
+                <img src="../../public/images/master.png" class="h-10 pt-2" />
+              </div>
+              <div class="flex justify-center">
+                <img src="../../public/images/line.png" class="h-9 pt-2" />
+              </div>
+              <div class="flex justify-center">
+                <img src="../../public/images/AirPay_Logo-.png" class="h-12" />
+              </div>
+              <div>
+                <img
+                  src="../../public/images//truemoney.png"
+                  class="h-10 pt-1"
+                />
+              </div>
+            </div>
+            <div class="flex justify-center items-end text-xs pt-2">
+              <div>Copyright © 2023 E-Book Alumilize. All rights reserved.</div>
+            </div>
+          </div>
+          <div class="col-span-2">
+            <div class="grid grid-rows-3">
+              <div class="text-sm flex justify-center pt-1 pb-0.5">REF</div>
+              <div class="grid grid-cols-3 row-span-2">
+                <div class="flex justify-center items-center">
+                  <a href="https://www.naiin.com" target="_blank">
+                  <img src="../../public/images/niin.png" class="h-12 cursor-pointer" />
+                  </a>
+                </div>
+                <div class="flex justify-center items-center">
+                  <a href="https://www.webtoons.com/th/" target="_blank">
+                  <img src="../../public/images/webtoon.png" class="h-12 cursor-pointer" />
+                  </a>
+                </div>
+                <div class="flex justify-center items-center">
+                  <a href="https://www.mebmarket.com/" target="_blank">
+                  <img src="../../public/images/meb.png" class="h-12 cursor-pointer" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="lg:hidden">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
