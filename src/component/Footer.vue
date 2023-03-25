1a23ad52a7d476735009bb2205ac5a11b6967701
nonybueno <88123596+nonybueno@users.noreply.github.com>
2023-03-26 03:12:53 +0700

    :lipstick: Footer

--- src/component/Footer.vue
+++ src/component/Footer.vue
@@ -2,40 +2,43 @@
 
 <template>
   <div>
-    <div class="w-full bg-yellow-500 h-64 flex justify-center">
-      <div class="w-4/6 grid grid-rows-3 max-lg:hidden pb-2">
+    <div class="w-full bg-yellow-500 h-64 flex justify-center max-lg:hidden">
+      <div class="w-4/6 grid grid-rows-3 pb-2">
         <div class="row-span-2">
-          <div class="grid grid-cols-4">
+          <!-- <div class="grid grid-cols-4">
             <div class="flex justify-end items-end h-40">
               <img src="../../public/images/footerLogo.png" class="h-28" />
-            </div>
-            <div
-              class="flex justify-center items-center col-span-2 pt-5 relative"
-            >
+            </div> -->
+          <div
+            class="flex justify-center items-center col-span-2 pt-5 relative"
+          >
+            <img
+              src="../../public/images/black-butler2-transformed.png"
+              class="h-36"
+            />
+            <div class="flex justify-center items-center pb-5">
               <img
-                src="../../public/images/black-butler2-transformed.png"
-                class="h-36"
+                src="../../public/images/qr-code.png"
+                class="absolute h-28 drop-shadow-xl"
               />
-              <div class="flex justify-center items-center pb-5">
-                <img
-                  src="../../public/images/qr-code.png"
-                  class="absolute h-28 drop-shadow-xl"
-                />
-              </div>
-            </div>
-            <div class="flex justify-center items-center text-lg">
-              <div>นอนอ่าน e-book สนุกกว่า!</div>
             </div>
           </div>
+          <!-- <div class="flex justify-center items-center text-lg">
+                <div>นอนอ่าน e-book สนุกกว่า!</div>
+              </div>
+            </div> -->
           <hr class="border-gray-800" />
         </div>
         <div class="grid grid-cols-10">
           <div class="col-span-2 flex justify-center items-center">
             <div>
               <div class="text-sm">Github Repo</div>
-              <div class="flex justify-center">
-                <a href="https://github.com/Nine0512/PROJECT2-SEC-2-Alumilize.git" target="_blank">
-                <img src="../../public/images/25231.png" class="h-14 w-14" />
+              <div class="flex justify-center hover:scale-110">
+                <a
+                  href="https://github.com/Nine0512/PROJECT2-SEC-2-Alumilize.git"
+                  target="_blank"
+                >
+                  <img src="../../public/images/25231.png" class="h-14 w-14" />
                 </a>
               </div>
             </div>
@@ -74,20 +77,29 @@
           <div class="col-span-2">
             <div class="grid grid-rows-3">
               <div class="text-sm flex justify-center pt-1 pb-0.5">REF</div>
-              <div class="grid grid-cols-3 row-span-2">
-                <div class="flex justify-center items-center">
+              <div class="grid grid-cols-3 row-span-2 gap-2">
+                <div class="flex justify-center items-center hover:scale-110">
                   <a href="https://www.naiin.com" target="_blank">
-                  <img src="../../public/images/niin.png" class="h-12 cursor-pointer" />
+                    <img
+                      src="../../public/images/niin.png"
+                      class="h-12 cursor-pointer"
+                    />
                   </a>
                 </div>
-                <div class="flex justify-center items-center">
+                <div class="flex justify-center items-center hover:scale-110">
                   <a href="https://www.webtoons.com/th/" target="_blank">
-                  <img src="../../public/images/webtoon.png" class="h-12 cursor-pointer" />
+                    <img
+                      src="../../public/images/webtoon.png"
+                      class="h-12 cursor-pointer"
+                    />
                   </a>
                 </div>
-                <div class="flex justify-center items-center">
+                <div class="flex justify-center items-center hover:scale-110">
                   <a href="https://www.mebmarket.com/" target="_blank">
-                  <img src="../../public/images/meb.png" class="h-12 cursor-pointer" />
+                    <img
+                      src="../../public/images/meb.png"
+                      class="h-12 cursor-pointer"
+                    />
                   </a>
                 </div>
               </div>
@@ -95,9 +107,73 @@
           </div>
         </div>
       </div>
+      </div>
 
-      <div class="lg:hidden">
-
+      <div class="w-full bg-yellow-500 h-52 flex justify-center border lg:hidden">
+      <div class="w-full flex justify-center text-white items-center border">
+        <div class="w-full grid grid-cols-10 gap-2 items-center">
+          <div class="col-span-2 flex justify-center">
+            <img src="../../public/images/footerLogo.png" class="h-36 pt-8" />
+          </div>
+          <div class="col-span-8 grid grid-cols-9 ml-6">
+            <div class="col-span-3 grid-row-3">
+              <div class="text-bold text-blue-900 pl-2">Repo</div>
+              <div class="hover:scale-110">
+                <a
+                  href="https://github.com/Nine0512/PROJECT2-SEC-2-Alumilize.git"
+                  target="_blank"
+                >
+                  About Us
+                </a>
+              </div>
+              <div
+                class="flex justify-center items-center hover:drop-shadow-xl w-14 mt-3 hover:scale-110"
+              >
+                <a
+                  href="https://github.com/Nine0512/PROJECT2-SEC-2-Alumilize.git"
+                  target="_blank"
+                >
+                  <img src="../../public/images/25231.png" class="h-14 w-14" />
+                </a>
+              </div>
+            </div>
+            <div class="col-span-3 grid-row-4">
+              <div class="text-bold text-blue-800 pl-2">REF</div>
+              <div class="hover:scale-110">
+                <a href="https://www.mebmarket.com/" target="_blank"> MEB </a>
+              </div>
+              <div class="hover:scale-110">
+                <a href="https://www.webtoons.com/th/" target="_blank">
+                  webtoon
+                </a>
+              </div>
+              <div class="hover:scale-110">
+                <a href="https://www.naiin.com" target="_blank"> Niin </a>
+              </div>
+            </div>
+            <div class="col-span-3 grid-row-6">
+              <div class="text-bold pl-2 text-blue-900">Member</div>
+              <div class="hover:scale-110">
+                <a href="https://github.com/win2114" target="_blank"> Winny </a>
+              </div>
+              <div class="hover:scale-110">
+                <a href="https://github.com/c3bosskung" target="_blank"> Bossy </a>
+              </div>
+              <div class="hover:scale-110">
+                <a href="https://github.com/nonybueno" target="_blank"> Nonny </a>
+              </div>
+              <div class="hover:scale-110">
+                <a href="https://github.com/Nine0512" target="_blank"> Ninny </a>
+              </div>
+              <div class="hover:scale-110">
+                <a href="https://github.com/Preeyapornn" target="_blank"> Benzy </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-span-10 flex justify-center">
+            Copyright © 2023 E-Book Alumilize. All rights reserved.
+          </div>
+        </div>
       </div>
     </div>
   </div>
