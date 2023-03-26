2ae95dc4934b0994ef0365dc5e7cd9a937e8acb9
nonybueno <88123596+nonybueno@users.noreply.github.com>
2023-03-26 16:25:25 +0700

    :lipstick: Footer

--- src/component/Footer.vue
+++ src/component/Footer.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div>
-    <div class="w-full bg-yellow-500 h-64 flex justify-center max-lg:hidden">
+    <div class="w-full bg-yellow-500 h-64 flex justify-center max-md:hidden">
       <div class="w-4/6 grid grid-rows-3 pb-2">
         <div class="row-span-2">
           <!-- <div class="grid grid-cols-4">
@@ -13,12 +13,12 @@
             class="flex justify-center items-center col-span-2 pt-5 relative"
           >
             <img
-              src="../../public/images/black-butler2-transformed.png"
+              src="images/black-butler2-transformed.png"
               class="h-36"
             />
             <div class="flex justify-center items-center pb-5">
               <img
-                src="../../public/images/qr-code.png"
+                src="images/qr-code.png"
                 class="absolute h-28 drop-shadow-xl"
               />
             </div>
@@ -38,7 +38,7 @@
                   href="https://github.com/Nine0512/PROJECT2-SEC-2-Alumilize.git"
                   target="_blank"
                 >
-                  <img src="../../public/images/25231.png" class="h-14 w-14" />
+                  <img src="images/25231.png" class="h-14 w-14" />
                 </a>
               </div>
             </div>
@@ -50,22 +50,22 @@
               </div>
               <div class="flex justify-center">
                 <img
-                  src="../../public/images/payment.png"
+                  src="images/payment.png"
                   class="h-9 pt-2 w-20 pl-1"
                 />
               </div>
               <div class="flex justify-center">
-                <img src="../../public/images/master.png" class="h-10 pt-2" />
+                <img src="images/master.png" class="h-10 pt-2" />
               </div>
               <div class="flex justify-center">
-                <img src="../../public/images/line.png" class="h-9 pt-2" />
+                <img src="images/line.png" class="h-9 pt-2" />
               </div>
               <div class="flex justify-center">
-                <img src="../../public/images/AirPay_Logo-.png" class="h-12" />
+                <img src="images/AirPay_Logo-.png" class="h-12" />
               </div>
               <div>
                 <img
-                  src="../../public/images//truemoney.png"
+                  src="images//truemoney.png"
                   class="h-10 pt-1"
                 />
               </div>
@@ -81,7 +81,7 @@
                 <div class="flex justify-center items-center hover:scale-110">
                   <a href="https://www.naiin.com" target="_blank">
                     <img
-                      src="../../public/images/niin.png"
+                      src="images/niin.png"
                       class="h-12 cursor-pointer"
                     />
                   </a>
@@ -89,7 +89,7 @@
                 <div class="flex justify-center items-center hover:scale-110">
                   <a href="https://www.webtoons.com/th/" target="_blank">
                     <img
-                      src="../../public/images/webtoon.png"
+                      src="images/webtoon.png"
                       class="h-12 cursor-pointer"
                     />
                   </a>
@@ -97,7 +97,7 @@
                 <div class="flex justify-center items-center hover:scale-110">
                   <a href="https://www.mebmarket.com/" target="_blank">
                     <img
-                      src="../../public/images/meb.png"
+                      src="images/meb.png"
                       class="h-12 cursor-pointer"
                     />
                   </a>
@@ -109,15 +109,15 @@
       </div>
       </div>
 
-      <div class="w-full bg-yellow-500 h-52 flex justify-center border lg:hidden">
-      <div class="w-full flex justify-center text-white items-center border">
-        <div class="w-full grid grid-cols-10 gap-2 items-center">
-          <div class="col-span-2 flex justify-center">
-            <img src="../../public/images/footerLogo.png" class="h-36 pt-8" />
+      <div class="w-full bg-yellow-500 sm:h-44 flex justify-center md:hidden">
+      <div class="w-full flex justify-center text-white items-center">
+        <div class="w-full grid grid-cols-10 sm:gap-2 items-center">
+          <div class="col-span-2 flex justify-center max-[600px]:ml-5">
+            <img src="images/footerLogo.png" class="h-32 max-[600px]:h-20 max-[600px]:w-16" />
           </div>
-          <div class="col-span-8 grid grid-cols-9 ml-6">
+          <div class="col-span-8 grid grid-cols-9 ml-6 max-[600px]:text-xs">
             <div class="col-span-3 grid-row-3">
-              <div class="text-bold text-blue-900 pl-2">Repo</div>
+              <div class="text-bold text-blue-900 pl-2 ">Repo</div>
               <div class="hover:scale-110">
                 <a
                   href="https://github.com/Nine0512/PROJECT2-SEC-2-Alumilize.git"
@@ -133,7 +133,7 @@
                   href="https://github.com/Nine0512/PROJECT2-SEC-2-Alumilize.git"
                   target="_blank"
                 >
-                  <img src="../../public/images/25231.png" class="h-14 w-14" />
+                  <img src="images/25231.png" class="h-14 w-14 max-[600px]:h-10 max-[600px]:w-10" />
                 </a>
               </div>
             </div>
@@ -170,7 +170,7 @@
               </div>
             </div>
           </div>
-          <div class="col-span-10 flex justify-center">
+          <div class="col-span-10 flex justify-center text-xs md:text-md max-[400px]:text-[5px]">
             Copyright © 2023 E-Book Alumilize. All rights reserved.
           </div>
         </div>
